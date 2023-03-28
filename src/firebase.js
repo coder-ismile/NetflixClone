188900658f8e285bb2047c10ec67d013701c45a8
coder-ismile <118710473+coder-ismile@users.noreply.github.com>
2023-03-28 08:34:23 +0530

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrGrn282POWO7JY5adrHREzRp3Nh-vKBg",
-  authDomain: "netflix-react-clone-74710.firebaseapp.com",
-  projectId: "netflix-react-clone-74710",
-  storageBucket: "netflix-react-clone-74710.appspot.com",
-  messagingSenderId: "926427697122",
-  appId: "1:926427697122:web:a02d1aef802779a019b3bb"
+  apiKey: "AIzaSyAyZFV0Scb7eLIVsI6h0VyHsPoreDZIGm8",
+  authDomain: "netflixclone-app-b1611.firebaseapp.com",
+  projectId: "netflixclone-app-b1611",
+  storageBucket: "netflixclone-app-b1611.appspot.com",
+  messagingSenderId: "19905285589",
+  appId: "1:19905285589:web:db593f643856aa3fca415c"
 };
 
 
